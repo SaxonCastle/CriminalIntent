193485ff7e28940be3eb0449ddc6c3b8b7daf182
Saxon Castle <swc006@student.usc.edu.au>
2020-08-07 19:46:57 +1000

    Completed ch 7 Fragments

--- app/src/main/java/com/bignerdranch/android/CrimeActivity.java
+++ app/src/main/java/com/bignerdranch/android/CrimeActivity.java
@@ -1,6 +1,8 @@
 package com.bignerdranch.android;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
@@ -10,5 +12,16 @@ public class CrimeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        //Fragment transactions are used to add, remove, attach, detach and replace fragments in the fragment list.
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new CrimeFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment) //Notice this fragment is a new CrimeFragment() object
+                    .commit();
+        }
     }
 }
--- app/src/main/java/com/bignerdranch/android/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/CrimeFragment.java
@@ -1,15 +1,34 @@
 package com.bignerdranch.android;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
 import androidx.fragment.app.Fragment;
 
+import static android.widget.CompoundButton.*;
+
 public class CrimeFragment extends Fragment {
 
+    //Initialise an object of the Crime class
     private Crime mCrime;
+
+    //Initialise the EditTexts
+    private EditText mTitleField;
+
+    //Initialise the Buttons
+    private Button mDateButton;
+
+    //Initialise the CheckBoxes
+    private CheckBox mSolvedCheckBox;
 
     /**
      * Notice this is public because they will be called by whatever activity is hosting the fragment
@@ -23,6 +42,7 @@ public class CrimeFragment extends Fragment {
 
     /**
      * A method to inflate the fragment in the activity that wants to host it.
+     * After the view is inflated, get a reference to the needed Views and add listeners to necessary members.
      *
      * @param inflater The activities method to call and host this fragment and points to its layout file.
      * @param container the views parent, needed to configure the widgets properly.
@@ -35,6 +55,41 @@ public class CrimeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_crime, container, false);
+
+        //Start mTitleField EditText
+        //The fragment class does not have a corresponding convenience method, so you have to call the real thing.
+        mTitleField = v.findViewById(R.id.crime_title);
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                //this space is intentionally left blank
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mCrime.setTitle(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                //This is intentionally left blank
+            }
+        });
+
+        //Start mDateButton Button
+        mDateButton = v.findViewById(R.id.crime_date);
+        mDateButton.setText(mCrime.getDate().toString());
+        //Disabling the button ensures that it will not respond
+        mDateButton.setEnabled(false);
+
+        //Start mSolvedCheckBox CheckBox
+        mSolvedCheckBox = v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mCrime.setSolved(isChecked);
+            }
+        });
         return v;
     }
 }
